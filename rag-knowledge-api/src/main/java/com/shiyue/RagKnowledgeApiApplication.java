1aab16a15b9b9bc9e990ffddc227a3915abab76a
ShiYue1026 <shiyue20011026@126.com>
2025-05-23 22:04:15 +0800

    initial commit

---
+++ rag-knowledge-api/src/main/java/com/shiyue/RagKnowledgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.shiyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagKnowledgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagKnowledgeApiApplication.class, args);
+	}
+
+}
---
+++ rag-knowledge-app/src/test/java/com/shiyue/RagKnowledgeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiyue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagKnowledgeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
